1e927dbf7542fd220db2733ed08cf2d8f04e4081
Nick Pang <48924710+NicoPang@users.noreply.github.com>
2023-10-08 16:10:23 -0400

    MF w/ Biases

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 import torch
 import torch.nn as nn
@@ -10,7 +11,7 @@ from skorch import NeuralNetRegressor
 BATCH_SIZE = 64
 NUM_FEATURES = 5
 TRAIN_RATIO = 0.8
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.0001
 EPOCHS = 20
 
 class MF(nn.Module):
@@ -18,11 +19,6 @@ class MF(nn.Module):
         super().__init__()
         self.user_m = nn.Embedding(n_users, K, dtype = torch.float64) # can include option sparse = True for memory
         self.item_m = nn.Embedding(n_users, K, dtype = torch.float64)
-        
-        self.user_b = nn.Embedding(n_users, 1, dtype = torch.float64)
-        self.item_b = nn.Embedding(n_items, 1, dtype = torch.float64)
-        nn.init.zeros_(self.user_b.weight)
-        nn.init.zeros_(self.item_b.weight)
     
     def forward(self, x):
         user_ids = x[:, 0]
@@ -30,12 +26,35 @@ class MF(nn.Module):
         user_embeds = self.user_m(user_ids)
         item_embeds = self.item_m(item_ids)
         prod = user_embeds * item_embeds
-        user_biases = self.user_b(user_ids).squeeze()
-        item_biases = self.item_b(item_ids).squeeze()
         
-        out = torch.sum(prod, 1) 	+ user_biases + item_biases
+        out = torch.sum(prod, 1)
         
         return out
+    
+# Matrix factorization with user/item biases
+class MF_Bias(MF):
+    def __init__(self, n_users, n_items, K, G_b):
+        super().__init__(n_users, n_items, K)
+
+        self.user_b = nn.Embedding(n_users, 1, dtype = torch.float64)
+        self.item_b = nn.Embedding(n_items, 1, dtype = torch.float64)
+        nn.init.zeros_(self.user_b.weight)
+        nn.init.zeros_(self.item_b.weight)
+
+        self.G_b = torch.from_numpy(G_b)
+
+    def forward(self, x):
+        user_ids = x[:, 0]
+        item_ids = x[:, 1]
+        out = super().forward(x)
+
+        user_biases = self.user_b(user_ids).squeeze()
+        item_biases = self.item_b(item_ids).squeeze()
+
+        out += user_biases + item_biases + self.G_b
+
+        return out
+
 
 if __name__ == '__main__':
     
@@ -62,9 +81,9 @@ if __name__ == '__main__':
     test_loader = DataLoader(test_dataset, batch_size = BATCH_SIZE)
     print('Dataloaders initialized.')
     
-    model = MF(U_size, I_size, NUM_FEATURES)
+    model = MF_Bias(U_size, I_size, NUM_FEATURES, G_b)
     loss_fn = nn.MSELoss
-    optimizer = optim.SGD
+    optimizer = optim.Adam
 
     regressor = NeuralNetRegressor(
         model,
