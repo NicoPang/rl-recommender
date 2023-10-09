4e94843e96c73ad3b4e0852cb1d430e9adb22788
Evil Snacks <Wantt0c0de912@tutanota.com>
2023-10-08 20:06:04 -0400

    I changed data =, so it can find numpy file.

--- src/test.py
+++ src/test.py
@@ -1,6 +1,5 @@
+import os
 import numpy as np
-import tensorflow as tf
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -14,30 +13,34 @@ TRAIN_RATIO = 0.8
 LEARNING_RATE = 0.0001
 EPOCHS = 20
 
+
 class MF(nn.Module):
     def __init__(self, n_users, n_items, K):
         super().__init__()
-        self.user_m = nn.Embedding(n_users, K, dtype = torch.float64) # can include option sparse = True for memory
-        self.item_m = nn.Embedding(n_users, K, dtype = torch.float64)
-    
+        self.user_m = nn.Embedding(
+            n_users, K, dtype=torch.float64
+        )  # can include option sparse = True for memory
+        self.item_m = nn.Embedding(n_users, K, dtype=torch.float64)
+
     def forward(self, x):
         user_ids = x[:, 0]
         item_ids = x[:, 1]
         user_embeds = self.user_m(user_ids)
         item_embeds = self.item_m(item_ids)
         prod = user_embeds * item_embeds
-        
+
         out = torch.sum(prod, 1)
-        
+
         return out
-    
+
+
 # Matrix factorization with user/item biases
 class MF_Bias(MF):
     def __init__(self, n_users, n_items, K, G_b):
         super().__init__(n_users, n_items, K)
 
-        self.user_b = nn.Embedding(n_users, 1, dtype = torch.float64)
-        self.item_b = nn.Embedding(n_items, 1, dtype = torch.float64)
+        self.user_b = nn.Embedding(n_users, 1, dtype=torch.float64)
+        self.item_b = nn.Embedding(n_items, 1, dtype=torch.float64)
         nn.init.zeros_(self.user_b.weight)
         nn.init.zeros_(self.item_b.weight)
 
@@ -56,41 +59,42 @@ class MF_Bias(MF):
         return out
 
 
-if __name__ == '__main__':
-    
-    data = np.load('../data/UIR_5core.npz')
-    x = data['x']
-    y = data['y']
-    U_size = data['U_size']
-    I_size = data['I_size']
-    G_b = data['G_b']
-    
+if __name__ == "__main__":
+    data = np.load(
+        os.path.abspath("../rl-recommender/datasets/processed/UIR_5core.npz")
+    )
+    x = data["x"]
+    y = data["y"]
+    U_size = data["U_size"]
+    I_size = data["I_size"]
+    G_b = data["G_b"]
+
     print(len(x))
     print(len(y))
     print(U_size)
     print(I_size)
     print(G_b)
-    
+
     dataset = TensorDataset(torch.from_numpy(x), torch.from_numpy(y))
     train_dataset, test_dataset = random_split(dataset, [TRAIN_RATIO, 1 - TRAIN_RATIO])
-    print(f'Training size: {len(train_dataset)}')
-    print(f'Testing size: {len(test_dataset)}')
-    print('Datasets loaded')
-    
-    train_loader = DataLoader(train_dataset, batch_size = BATCH_SIZE, shuffle = True)
-    test_loader = DataLoader(test_dataset, batch_size = BATCH_SIZE)
-    print('Dataloaders initialized.')
-    
+    print(f"Training size: {len(train_dataset)}")
+    print(f"Testing size: {len(test_dataset)}")
+    print("Datasets loaded")
+
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
+    print("Dataloaders initialized.")
+
     model = MF_Bias(U_size, I_size, NUM_FEATURES, G_b)
     loss_fn = nn.MSELoss
     optimizer = optim.Adam
 
     regressor = NeuralNetRegressor(
         model,
-        max_epochs = EPOCHS,
-        criterion = loss_fn,
-        optimizer = optimizer,
-        optimizer__lr = LEARNING_RATE
+        max_epochs=EPOCHS,
+        criterion=loss_fn,
+        optimizer=optimizer,
+        optimizer__lr=LEARNING_RATE,
     )
-    
+
     regressor.fit(x, y)
