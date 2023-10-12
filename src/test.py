61c84f8d8e5ac86b801de2e1bf50621dcde5b38b
Nick Pang <48924710+NicoPang@users.noreply.github.com>
2023-10-11 22:26:52 -0400

    Implemented saving/loading as a function.

--- src/test.py
+++ src/test.py
@@ -2,18 +2,20 @@ import numpy as np
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
+from torch.optim import Adam
 from torch.utils.data import random_split, TensorDataset, DataLoader
 
 from skorch import NeuralNetRegressor
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 from model.mf import MF_Bias
+from model.utility import RMSELoss
+from train.saving import save_model_results
 
 BATCH_SIZE = 64
 NUM_FEATURES = 5
-TRAIN_RATIO = 0.8
 LEARNING_RATE = 0.0001
-EPOCHS = 20
+EPOCHS = 2
 
 if __name__ == '__main__':
     
@@ -29,27 +31,21 @@ if __name__ == '__main__':
     print(U_size)
     print(I_size)
     print(G_b)
-    
-    dataset = TensorDataset(torch.from_numpy(x), torch.from_numpy(y))
-    train_dataset, test_dataset = random_split(dataset, [TRAIN_RATIO, 1 - TRAIN_RATIO])
-    print(f'Training size: {len(train_dataset)}')
-    print(f'Testing size: {len(test_dataset)}')
-    print('Datasets loaded')
-    
-    train_loader = DataLoader(train_dataset, batch_size = BATCH_SIZE, shuffle = True)
-    test_loader = DataLoader(test_dataset, batch_size = BATCH_SIZE)
-    print('Dataloaders initialized.')
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+    y_train, y_test = torch.tensor(y_train, dtype = torch.float64), torch.tensor(y_test, dtype = torch.float64)
     
     model = MF_Bias(U_size, I_size, NUM_FEATURES, G_b)
-    loss_fn = nn.MSELoss
-    optimizer = optim.Adam
+    loss_fn = RMSELoss()
+    optimizer = Adam
 
     regressor = NeuralNetRegressor(
         model,
-        max_epochs = EPOCHS,
         criterion = loss_fn,
         optimizer = optimizer,
-        optimizer__lr = LEARNING_RATE
+        optimizer__lr = LEARNING_RATE,
+        batch_size = BATCH_SIZE,
+        max_epochs = EPOCHS
     )
     
-    regressor.fit(x, y)
+    save_model_results(regressor, x, y, f'../results/bs{BATCH_SIZE}K{NUM_FEATURES}LR{LEARNING_RATE}.npz')
---
+++ src/train/saving.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Not for Cross-Validation
+def save_model_results(model, x, y, filepath, save_params = False):
+    # TODO: implement model parameter saving
+    results = model.fit(x, y)
+    history = results.history
+
+    tr_losses = [i['train_loss'] for i in history]
+    t_losses = [i['valid_loss'] for i in history]
+
+    np.savez_compressed(filepath, tr_loss = tr_losses, t_loss = t_losses)
