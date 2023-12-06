e74c727f6d70e8e533052d206661293a026a6528
John Bogacz <Wantt0c0de912@tutanota.com>
2023-12-05 21:06:52 -0500

    hi

--- src/data/LDA.py
+++ src/data/LDA.py
@@ -13,7 +13,7 @@ nlp = spacy.load("en_core_web_sm")
 nlp = spacy.load("es_core_news_sm")
 
 
-def create_topics(df: pd.DataFrame, s: str, n: int):
+def create_topics(pth: str, df: pd.DataFrame, s: str, n: int):
     if (s != "User_ID") and (s != "Item_ID"):
         raise Exception(f"{s} is not Item_ID or User_ID")
     if n <= 0:
@@ -50,7 +50,9 @@ def create_topics(df: pd.DataFrame, s: str, n: int):
     document_topics = lda.transform(X)
     tf_feature_names = vectorizer.get_feature_names_out()
 
-    with open(f"{s}_topics.txt", "w") as f_file:
+    with open(
+        os.path.join(pth, "datasets", "processed", "lda", f"{s}_topics.txt"), "w"
+    ) as f_file:
         all_text = str()
         for i, topic in enumerate(lda.components_):
             l = f"Topic_{i}: "
@@ -105,14 +107,19 @@ def preprocess_text(dataset: set, pth: str, n_topics: int):
     total_df["Review_Body"] = total_df["Review_Body"].apply(
         lambda x: str(x).replace("\\", "")
     )
-    total_df.to_csv("total_df.csv.gz", compression="gzip", escapechar="\\")
+    total_df.to_csv(
+        os.path.join(pth, "datasets", "processed", "lda", "total_df.csv.gz"),
+        compression="gzip",
+        escapechar="\\",
+    )
 
     # (D) rename User_ID and Item_ID to index
     user_id_mapping = {
         user_id: idx for idx, user_id in enumerate(total_df["User_ID"].unique())
     }
     pd.DataFrame(data=user_id_mapping.keys()).to_csv(
-        "user_mappings.csv.gz", compression="gzip"
+        os.path.join(pth, "datasets", "processed", "lda", "user_mappings.csv.gz"),
+        compression="gzip",
     )
     total_df["User_ID"] = total_df["User_ID"].map(user_id_mapping)
     del user_id_mapping
@@ -121,18 +128,23 @@ def preprocess_text(dataset: set, pth: str, n_topics: int):
         user_id: idx for idx, user_id in enumerate(total_df["Item_ID"].unique())
     }
     pd.DataFrame(data=item_id_mapping.keys()).to_csv(
-        "item_mappings.csv.gz", compression="gzip"
+        os.path.join(pth, "datasets", "processed", "lda", "item_mappings.csv.gz"),
+        compression="gzip",
     )
     total_df["Item_ID"] = total_df["Item_ID"].map(item_id_mapping)
     del item_id_mapping
     gc.collect()
 
     # (E) Calculate topic distribution for each user and each item
-    Item_Topic = create_topics(total_df, s="Item_ID", n=n_topics)
-    Item_Topic.to_csv("item_topics.csv.gz", compression="gzip")
+    Item_Topic = create_topics(pth, total_df, s="Item_ID", n=n_topics)
+    Item_Topic.to_csv(
+        os.path.join(pth, "datasets", "processed", "lda", "item_topics.csv")
+    )
 
-    User_Topics = create_topics(total_df, s="User_ID", n=n_topics)
-    User_Topics.to_csv("user_topics.csv.gz", compression="gzip")
+    User_Topics = create_topics(pth, total_df, s="User_ID", n=n_topics)
+    User_Topics.to_csv(
+        os.path.join(pth, "datasets", "processed", "lda", "user_topics.csv")
+    )
 
 
 if __name__ == "__main__":
@@ -144,68 +156,3 @@ if __name__ == "__main__":
     pth = os.path.abspath(__file__)[:-16]
 
     preprocess_text(dataset=subsets, pth=pth, n_topics=10)
-
-    os._exit(-1)
-    preprocess_text
-    subsets = {
-        "Toys_and_Games_5.json",
-        "reviews_Toys_and_Games.json",
-        "Apps_for_Android_5.json",
-        "reviews_Apps_for_Android.json",
-        "Health_and_Personal_Care_5.json",
-        "reviews_Health_and_Personal_Care.json",
-    }
-    total_df = pd.DataFrame(
-        columns=["User_ID", "Item_ID", "Review_Body", "Review_Score", "Review_Header"]
-    )
-
-    # (1) Check if datasets on system
-    git_path = os.path.abspath(__file__)[
-        :-16
-    ]  # make sure not to change current filename or folder names
-    for name in subsets:
-        if "5" not in name:
-            continue
-        p = os.path.join(git_path, "datasets", "raw", name)
-        if os.path.exists(path=p) is False:
-            print(f"ERROR: issue not finding unzipped file at {p}")
-
-        # (2) Combine the unzipped json datasets
-        df = pd.read_json(path_or_buf=p, lines=True)
-
-        df = df.drop(
-            ["reviewerName", "helpful", "unixReviewTime", "reviewTime"], axis=1
-        )
-        df = df.rename(
-            columns={
-                "reviewerID": "User_ID",
-                "asin": "Item_ID",
-                "reviewText": "Review_Body",
-                "summary": "Review_Header",
-                "overall": "Review_Score",
-            }
-        )
-        total_df = pd.concat([df, total_df], axis=0)
-
-    # (3) Create a mapping so we don't have to deal with random hashes for User_ID and Item_ID
-    user_id_mapping = {
-        user_id: idx for idx, user_id in enumerate(total_df["User_ID"].unique())
-    }
-    pd.DataFrame(data=user_id_mapping.keys()).to_excel("user_mappings.xlsx")
-    total_df["User_ID"] = total_df["User_ID"].map(user_id_mapping)
-
-    item_id_mapping = {
-        user_id: idx for idx, user_id in enumerate(total_df["Item_ID"].unique())
-    }
-    pd.DataFrame(data=item_id_mapping.keys()).to_excel("item_mappings.xlsx")
-    total_df["Item_ID"] = total_df["Item_ID"].map(item_id_mapping)
-
-    # (4) Testing LDA
-    Item_Topic = create_topics(total_df, s="Item_ID", n=10)
-    Item_Topic.to_excel("item_topics.xlsx")
-
-    User_Topics = create_topics(total_df, s="User_ID", n=10)
-    User_Topics.to_excel("user_topics.xlsx")
-    total_df.to_excel("total_df.xlsx")
-
-    total_df.head(1048576).to_excel("total_df_max.xlsx")
